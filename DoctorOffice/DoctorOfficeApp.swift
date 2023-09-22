44b9c8fbbfbaaa762f83e071103bc87c8940b9c7
Ali <alirezamonfared813@gmail.com>
2023-09-22 14:32:08 -0700

    Initial Commit

---
+++ DoctorOffice/DoctorOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorOfficeApp.swift
+//  DoctorOffice
+//
+//  Created by Alireza Karimi on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct DoctorOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
